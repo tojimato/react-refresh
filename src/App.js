56d6972c1d91e926f2121a5674038631bce38120
tojimato <tolga.kok@aforsoft.com>
2023-12-05 17:05:12 +0300

    basic components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import './App.css';
+import Todo from "./components/Todo";
 
 function App() {
   return (
-    <div>HELLO ! TOLGA </div>
+    <div>
+      <h1>My Todos</h1>
+      <Todo text='Topic 1' />
+      <Todo text='Topic 2' />
+      <Todo text='Topic 3' />
+ 
+    </div>
   );
 }
 
